726d3ae9095d4b3b87a110f4fcde401cf0707fa6
Son Chau <sonchau24@yahoo.com>
2021-02-15 12:27:25 +1100

    [#66] loading events

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -1,11 +1,13 @@
 import React from 'react';
 import './App.css';
 import Recorder from '../Recorder';
+import Calendar from '../Calendar';
 
 const App: React.FC = () => {
   return (
     <div className="App">
       <Recorder />
+      <Calendar />
     </div>
   );
 };
--- src/redux/store.ts
+++ src/redux/store.ts
@@ -1,4 +1,5 @@
-import { combineReducers, createStore } from 'redux';
+import { combineReducers, createStore, applyMiddleware } from 'redux';
+import thunk from 'redux-thunk';
 import userEventsReducer from './user-events';
 import recorderReducer from './recorder';
 
@@ -9,6 +10,6 @@ const rootReducer = combineReducers({
 
 export type RootState = ReturnType<typeof rootReducer>;
 
-const store = createStore(rootReducer);
+const store = createStore(rootReducer, applyMiddleware(thunk));
 
 export default store;
